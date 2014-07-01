fcd27c180d409295203e208c22d2e9ca390d3493
Joe Dowdell <joedowdell@gmail.com>
2014-07-01 18:23:18 +0100

    added save to csv tests

--- lib/directory.rb
+++ lib/directory.rb
@@ -1,18 +1,22 @@
+# def students
+# 	[
+# 	 {name: "Dr. Hannibal Lecter", cohort: "June"}, 
+# 	 {name: "Darth Vader", cohort: "January"},
+# 	 {name: "Nurse Ratched", cohort: "June"},
+# 	 {name: "Michael Corleone", cohort: "August"},
+# 	 {name: "Alex De Large", cohort: "June"},
+# 	 {name: "The Alien", cohort: "June"},
+# 	 {name: "Terminator", cohort: "July"},
+# 	 {name: "Freddy Kruger", cohort: "June"},
+# 	 {name: "Joe", cohort: "June"},
+# 	 {name: "Peter", cohort: "June"},
+# 	 {name: "Nikesh", cohort: "June"},
+# 	 {name: "The Joker", cohort: "June"}
+# 	]
+# end
+
 def students
-	[
-	 {name: "Dr. Hannibal Lecter", cohort: "June"}, 
-	 {name: "Darth Vader", cohort: "January"},
-	 {name: "Nurse Ratched", cohort: "June"},
-	 {name: "Michael Corleone", cohort: "August"},
-	 {name: "Alex De Large", cohort: "June"},
-	 {name: "The Alien", cohort: "June"},
-	 {name: "Terminator", cohort: "July"},
-	 {name: "Freddy Kruger", cohort: "June"},
-	 {name: "Joe", cohort: "June"},
-	 {name: "Peter", cohort: "June"},
-	 {name: "Nikesh", cohort: "June"},
-	 {name: "The Joker", cohort: "June"}
-	]
+	students = []
 end
 
 def print_header
@@ -40,11 +44,8 @@ def user_input
 			name = gets.chomp
 			user_input
 		end
+		students << name
 end
-
-# def name 
-# 	@students << name
-# end
 
 
 def print_footer
@@ -67,9 +68,30 @@ def students_count
 	students_count = students.count{ |student| students.length }
 end
 
+
+require 'csv'
+
+def student_to_csv(student)
+	student.values
+end
+
+def save(students)
+	CSV.open("./students.csv", "w") do |csv|
+		students.each do |student|
+			csv << student_to_csv(student)
+		end
+	end		
+end
+
+def load_csv
+	file = CSV.open("./students.csv", "r")
+
+
+end 
+
 print_header
 print_menu
 print_input_name
 user_input
-# print_students
+print_students
 print_footer
--- spec/directory_spec.rb
+++ spec/directory_spec.rb
@@ -3,65 +3,93 @@ require 'directory'
 #context 'This is how the program works' do
 
 	describe 'The student directory contains the following functions' do
-		it 'prints the header' do
-			header = 'This is the MA student directory June'
-      expect(self).to receive(:puts).with(header)
-      print_header
-		end		
+		let(:student) { { name: 'Joe', cohort: 'June' } }
+			it 'prints the header' do
+				header = 'This is the MA student directory June'
+	      expect(self).to receive(:puts).with(header)
+	      print_header
+			end		
 
-		it 'can print the menu' do
-			menu = [
-			'------MENU-------',
-			'1. Input the students',
-			"2. Show the students",
-			"3. Save the list to students.csv",
-			"4. Load the list from students.csv",
-			"9. Exit",
-			"--------------------"
-			]
+			it 'can print the menu' do
+				menu = [
+				'------MENU-------',
+				'1. Input the students',
+				"2. Show the students",
+				"3. Save the list to students.csv",
+				"4. Load the list from students.csv",
+				"9. Exit",
+				"--------------------"
+				]
 
-			menu.each { |line| expect(self).to receive(:puts).with(line)}
+				menu.each { |line| expect(self).to receive(:puts).with(line)}
 
-			print_menu
-		end
+				print_menu
+			end
 
-		it 'asks for user input' do
-			input_name = 'Please enter your name'
-			expect(self).to receive(:puts).with(input_name)
-			print_input_name
-		end
+			it 'asks for user input' do
+				input_name = 'Please enter your name'
+				expect(self).to receive(:puts).with(input_name)
+				print_input_name
+			end
 
-		it 'can take user input' do
-			expect(self).to receive(:gets).and_return("pete")
-			user_input
-		end
+			it 'can take user input' do
+				expect(self).to receive(:gets).and_return("pete")
+				user_input
+			end
 
-		it 'tells us the names of the students' do
-			names = students.map{|student| student[:name]}
-			expect(student_names).to eq names
-		end
+			# it 'adds the user input to the students list' do
+			# 	expect(user_input).to match 
+			# end
 
-		it 'can print student names with cohorts' do
-			expect(self).to receive(:puts).with(student_names.join(', '))
-			print_students
-		end
+			it 'tells us the names of the students' do
+				names = students.map{|student| student[:name]}
+				expect(student_names).to eq names
+			end
 
-		it 'tells us the cohort of the students' do
-			cohort = students.map{|student| student[:cohort]}
-			expect(student_cohort).to eq cohort
-		end
+			it 'can print student names with cohorts' do
+				expect(self).to receive(:puts).with(student_names.join(', '))
+				print_students
+			end
+
+			it 'tells us the cohort of the students' do
+				cohort = students.map{|student| student[:cohort]}
+				expect(student_cohort).to eq cohort
+			end
 
 
-		it 'prints the footer' do
-			footer = 'Overall we have ' + students.length.to_s + ' great students'
-			expect(self).to receive(:puts).with(footer)
-			print_footer
-		end
+			it 'prints the footer' do
+				footer = 'Overall we have ' + students.length.to_s + ' great students'
+				expect(self).to receive(:puts).with(footer)
+				print_footer
+			end
 
-		it 'counts the students' do
-			count = students_count{|student| students.length}
-			expect(students.length). to eq count
-		end
+			it 'counts the students' do
+				count = students_count{|student| students.length}
+				expect(students.length). to eq count
+			end
+
+
+			
+			it 'transforms a student into csv' do
+				expect(student_to_csv(student)).to eq ['Joe', 'June']
+			end
+
+			it 'saves the students' do
+				students = [student]
+				csv = double # double is fake file, like a stunt double.
+				expect(csv).to receive(:<<).with(student_to_csv(student))
+				expect(CSV).to receive(:open).with('./students.csv', 'w').and_yield(csv)
+				save(students)
+
+			end
+
+			it 'loads the students' do
+				students = [student]
+				expect(CSV).to receive(:open).with("./students.csv", "r")
+				load_csv
+			end
+		
+
 
 
 	end
